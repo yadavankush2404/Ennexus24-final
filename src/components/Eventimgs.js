d93d3fc0ce51931962503d9ba6c8f4cdc3fdaf26
Ankush Yadav <129288727+yadavankush2404@users.noreply.github.com>
2024-04-04 14:42:04 +0530

    Update Eventimgs.js

--- src/components/Eventimgs.js
+++ src/components/Eventimgs.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import img_1 from './assets/images/pici.jpg'
+import img_2 from './assets/images/m.jpg'
+import img_3 from './assets/images/o.jpg'
+import img_4 from './assets/images/c.jpg'
+
 
 function Eventimgs() {
-  const arr = [img_1,img_1,img_1,img_1];
+  const arr = [img_1,img_2,img_3,img_4];
   
     return (
       <>
